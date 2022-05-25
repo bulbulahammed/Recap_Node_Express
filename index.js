b758debce1e821506a61efab9628e8c92101b10e
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-05-25 17:02:52 +0600

    routing

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log(req);
-    res.end("Hello from server");
+    const pathName = req.url;
+    if (pathName === '/' || pathName === '/overview'){
+        res.end('This is overview');
+        }else if(pathName === '/product'){
+        res.end("This is product")
+    }else{
+        res.writeHead(404,{
+            'content-type': "text/html",
+            'my-own-header': 'My header'
+        });
+        res.end('<h1>Your requested page is not found</h1>');
+    }
 });
 
-server.listen(8000,'127.0.0.1'),()=>{
+server.listen(3000,'127.0.0.1'),()=>{
     console.log("Server is listening at port 8000 local host");
 }
