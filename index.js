5be1d4da47c6b77211ad045d63229a4dfd8fdebf
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-05-25 12:07:29 +0600

    write file system

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+// Node.js program to demonstrate the
+// fs.writeFileSync() method
+
+// Import the filesystem module
 const fs = require('fs');
 
-const text = fs.readFileSync('./read.txt','utf-8');
+// Writing to the file 5 times
+// with the append file mode
+for (let i = 0; i < 5; i++) {
+fs.writeFileSync("movies.txt",
+	"Movie " + i + "\n",
+	{
+	encoding: "utf8",
+	flag: "a+",
+	mode: 0o666
+	});
+}
 
-console.log(text);
+console.log("File written successfully 5 times\n");
+console.log("The written file has the following contents:");
+console.log(fs.readFileSync("movies.txt", "utf8"));
