6776a9b750f3ccf9cd7f082dec9d093d537adf61
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-05-28 20:29:31 +0600

    import own module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const replaceTemplate = require('./modules/replaceTemplate');
 
 
 const tempOverview = fs.readFileSync(`${__dirname}/template/overview.htm`,'utf-8');
@@ -11,21 +12,6 @@ const temProduct = fs.readFileSync(`${__dirname}/template/product.html`,'utf-8')
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8')
     const dataObj = JSON.parse(data);
-
-
-const replaceTemplate = (temp,product)=>{
- let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
- output = output.replace(/{%IMAGE%}/g,product.image);
- output = output.replace(/{%FROM%}/g,product.from);
- output = output.replace(/{%NUTRIENTS%}/g,product.nutrients);
- output = output.replace(/{%QUANTITY%}/g,product.quantity);
- output = output.replace(/{%PRICE%}/g,product.price);
- output = output.replace(/{%ID%}/g,product.id);
- output = output.replace(/{%DESCRIPTION%}/g,product.description);
- if(! product.organic) output = output.replace(/{%NOT_ORGANIC%}/g,"not-organic")
- return output;
-}
-
 
 // Creating server
 const server = http.createServer((req,res)=>{
